e022f2a47b0dc16d4da1ebeda98e099c7d0e3290
vivek <vivek2020bheke@gmail.com>
2021-12-16 18:46:19 +1300

    d

--- public/AdminView.js
+++ public/AdminView.js
@@ -1,6 +1,6 @@
 firebase.auth().onAuthStateChanged((user)=>{
     if(!user){
-        location.replace("home.html") // if user is not logged in , send to home page to login
+        location.replace("index.html") // if user is not logged in , send to home page to login
     }else{
         document.getElementById("user").innerHTML = "Hello, "+user.email  // if user is logged in , show add galaxz admin view
     }
